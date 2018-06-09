cb11d161b6aec7ee94aef251c0bf581183ad57fd
santaclose <santidetaramonaf@gmail.com>
2018-06-09 16:01:19 +0200

    Add files via upload

---
+++ blender/import.py
@@ -0,0 +1,96 @@
+import bpy
+
+def read_some_data(context, filepath, use_some_setting):
+    print("running read_some_data...")
+    f = open(filepath, 'r', encoding='utf-8')
+    data = f.read()
+    f.close()
+
+    lines = data.split('\n')
+    for line in lines:
+        if line[0] == 'o':
+            name = ""
+            i = 1
+            while line[i] != '[':
+                i += 1
+
+            i += 1
+            while line[i] != ']':
+                name += line[i]
+                i += 1
+
+            mesh = bpy.data.meshes.new("mesh")  # add a new mesh
+            object = bpy.data.objects.new(name, mesh)  # add a new object using the mesh
+            scene = bpy.context.scene
+            scene.objects.link(object)  # put the object into the scene (link)
+            scene.objects.active = object  # set as the active object in the scene
+            object.select = True  # select object
+            mesh = bpy.context.object.data
+            bm = bmesh.new()
+        elif line[0]
+
+
+    return {'FINISHED'}
+
+
+# ImportHelper is a helper class, defines filename and
+# invoke() function which calls the file selector.
+from bpy_extras.io_utils import ImportHelper
+from bpy.props import StringProperty, BoolProperty, EnumProperty
+from bpy.types import Operator
+
+
+class ImportSomeData(Operator, ImportHelper):
+    """This appears in the tooltip of the operator and in the generated docs"""
+    bl_idname = "import_test.some_data"  # important since its how bpy.ops.import_test.some_data is constructed
+    bl_label = "Import Some Data"
+
+    # ImportHelper mixin class uses this
+    filename_ext = ".txt"
+
+    filter_glob = StringProperty(
+            default="*.txt",
+            options={'HIDDEN'},
+            maxlen=255,  # Max internal buffer length, longer would be clamped.
+            )
+
+    # List of operator properties, the attributes will be assigned
+    # to the class instance from the operator settings before calling.
+    use_setting = BoolProperty(
+            name="Example Boolean",
+            description="Example Tooltip",
+            default=True,
+            )
+
+    type = EnumProperty(
+            name="Example Enum",
+            description="Choose between two items",
+            items=(('OPT_A', "First Option", "Description one"),
+                   ('OPT_B', "Second Option", "Description two")),
+            default='OPT_A',
+            )
+
+    def execute(self, context):
+        return read_some_data(context, self.filepath, self.use_setting)
+
+
+# Only needed if you want to add into a dynamic menu
+def menu_func_import(self, context):
+    self.layout.operator(ImportSomeData.bl_idname, text="Text Import Operator")
+
+
+def register():
+    bpy.utils.register_class(ImportSomeData)
+    bpy.types.INFO_MT_file_import.append(menu_func_import)
+
+
+def unregister():
+    bpy.utils.unregister_class(ImportSomeData)
+    bpy.types.INFO_MT_file_import.remove(menu_func_import)
+
+
+if __name__ == "__main__":
+    register()
+
+    # test call
+    bpy.ops.import_test.some_data('INVOKE_DEFAULT')
---
+++ blender/script.py
@@ -0,0 +1,53 @@
+
+import bpy, bmesh, random, math
+from mathutils import Vector
+
+def createObject(name):
+    global bm
+    global mesh
+    mesh = bpy.data.meshes.new("mesh")  # add a new mesh
+    object = bpy.data.objects.new(name, mesh)  # add a new object using the mesh
+    scene = bpy.context.scene
+    scene.objects.link(object)  # put the object into the scene (link)
+    scene.objects.active = object  # set as the active object in the scene
+    object.select = True  # select object
+    mesh = bpy.context.object.data
+    bm = bmesh.new()
+    return
+
+filepath = "C:\\Users\\santi\\Desktop\\ofv\\sample.ofv"
+
+f = open(filepath, 'r', encoding='utf-8')
+data = f.read()
+f.close()
+
+objectCreated = False
+verticesDic = {} # name, bm.vert
+lines = data.split('\n')
+
+for line in lines:
+
+    if line[0] == 'o':
+        if objectCreated:
+            bm.to_mesh(mesh)
+            bm.free()
+        name = line[line.index('[') + 1 : line.index(']')]
+        createObject(name)
+        objectCreated = True
+
+    elif line[0] == 'v':
+        split = line.split('[')
+        inBrackets = split[1][0 : split[1].index(']')]
+        ss = inBrackets.split(',')
+        verticesDic[split[0]] = bm.verts.new(Vector((float(ss[0]),float(ss[1]),float(ss[2]))))
+
+    elif line[0] == 'f':
+        string = line[line.index('[') + 1 : line.index(']')]
+        split = string.split(',')
+        verts = []
+        for a in split:
+            verts.append(verticesDic[a])
+        bm.faces.new(verts)
+
+bm.to_mesh(mesh) # make the bmesh the object's mesh
+bm.free()  # always do this when finished
